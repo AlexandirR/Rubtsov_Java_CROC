fb143a19a7aa80f274ef7cee4e24e275c5f79c3a
Alexandr <rubtsov.a.p@edu.mirea.ru>
2021-10-18 19:50:30 +0300

    исправлена 3 задача

--- week1/src/ThirdEx/SwapInMass.java
+++ week1/src/ThirdEx/SwapInMass.java
@@ -5,28 +5,57 @@ import java.util.Scanner;
 
 public class SwapInMass {
 
-    public static void main(String[] args) {
-        String input;
-        Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
+    private static String input;
+    private static int[] mas;
+
+    // Метод считывания массива
+    public static void inputMass() {
         input = scanner.nextLine();
-        int[] mas = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-        int maxI = 0, minI = 0;
-        for(int i = 0; i < mas.length; ++i) {
-            if(mas[i] > mas[maxI]) {
-                maxI = i;
-            }
+        mas = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray(); //парс строки в масив интов
+    }
+
+    // вывод массива
+    public static void outputMass() {
+        for (int temp: mas) {
+            System.out.print(temp + " ");
+        }
+    }
+
+    // Метод для поиска индексов максимального и минимальнго значения
+    public static int findIndexOfMin() {
+        int minI = 0;
+        for(int i = 1; i < mas.length; ++i) {
             if(mas[i] < mas[minI]) {
                 minI = i;
             }
         }
-        int t = mas[0];
-        mas[0] = mas[minI];
-        mas[minI] = t;
-        t = mas[mas.length - 1];
-        mas[mas.length - 1] = mas[maxI];
-        mas[maxI] = t;
-        for (int temp: mas) {
-            System.out.print(temp + " ");
+        return minI;
+    }
+
+    public static int findIndexOfMax() {
+        int maxI = 0;
+        for(int i = 1; i < mas.length; ++i) {
+            if(mas[i] >= mas[maxI]) {
+                maxI = i;
+            }
         }
+        return maxI;
+    }
+
+    // Метод для обмента двух элементов местами
+    public static void swap(int indexFirst, int indexSecond) {
+        int t = mas[indexFirst];
+        mas[indexFirst] = mas[indexSecond];
+        mas[indexSecond] = t;
+    }
+
+    // Основной метод
+    public static void main(String[] args) {
+        inputMass();
+        //Нахождение индексов мин и макс и постановка их в начало и конеу массива соотвественно
+        swap(0, findIndexOfMin());
+        swap(mas.length - 1, findIndexOfMax());
+        outputMass();
     }
 }
