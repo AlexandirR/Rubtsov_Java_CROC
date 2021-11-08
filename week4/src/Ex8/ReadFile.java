2171775b01720941d05f2bded20cad00bc694c05
Alexandr <rubtsov.a.p@edu.mirea.ru>
2021-11-08 17:36:46 +0300

    Изменения в 8 задаче

--- week4/src/Ex8/ReadFile.java
+++ week4/src/Ex8/ReadFile.java
@@ -3,17 +3,34 @@ package Ex8;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ReadFile {
 
+    //Слово - любой символьный блок имеющий хоть одну букву или цифру отделенный пробелами или переводами строк
     public static int countWords(String pathFile) throws FileNotFoundException {
         int count = 0;
         File file = new File(pathFile);
+        String temp;
+        boolean flag;
         try(Scanner sc = new Scanner(new FileInputStream(file))) {
             while (sc.hasNext()) {
-                sc.next();
-                count++;
+                temp = sc.next();
+                flag = false;
+                for(int i = 0; i < temp.length(); ++i) {
+                    char c = temp.charAt(i);
+                    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+                            (c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я') ||
+                            (c >= '0' && c <= '9')) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag) {
+                    count++;
+                }
             }
         }
         return count;
